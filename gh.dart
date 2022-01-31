2a15455f11edc1768971d81fc2236312c7066fa1
zackmomodu <zackmomodu@gmail.com>
2022-01-31 14:06:34 -0800

    my first commit

---
+++ gh.dart
@@ -0,0 +1,7 @@
+//@dart=2.10
+import "dart:io";
+void main()[
+String course="Dart programming";
+print(course);
+
+]
---
+++ greeting.dart
@@ -0,0 +1,11 @@
+//@dart=2.10
+import "dart:io";
+void main(){
+  print("input name");
+  String name = stdin.readLineSync();
+  print("input department");
+  String department = stdin.readLineSync();
+  print("cgpa");
+  String cgpa = stdin.readLineSync();
+  print("Hello $name, you graduated with $cgpa from the department of $department");
+}
